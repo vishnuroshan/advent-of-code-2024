e03a3d2e73f725b3a024a5f837a4093d360a1c18
Vishnu Roshan <32105473+vishnuroshan@users.noreply.github.com>
2024-12-02 20:26:01 +0530

    Update solution_part_1.js

--- challenge_2/solution_part_1.js
+++ challenge_2/solution_part_1.js
@@ -6,24 +6,39 @@ const challenge2 = () => {
   return arr;
 };
 
-function getSafeReports() {
-  const array = challenge2();
-
-  const safeReports = [];
-  array.forEach((report, i) => {
-    let isSafe = [];
-    let sign;
-    for (let i = 0; i < report.length; i++) {
-      if (report[i + 1] !== undefined) {
-        const diff = report[i] - report[i + 1];
-        if (!sign) sign = Math.sign(diff);
-        if (sign !== Math.sign(diff)) isSafe.push(false);
-        if (diff !== 0 && diff >= -3 && diff <= 3) isSafe.push(true);
-        else isSafe.push(false);
+function checkSafetyForArray(array) {
+  let arr = [...array];
+  const isSafe = [];
+  let sign;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 1] !== undefined) {
+      const diff = arr[i] - arr[i + 1];
+      // if no sign assign
+      if (!sign) sign = Math.sign(diff);
+      if (sign !== Math.sign(diff)) {
+        // if sign changes, then its not safe
+        isSafe.push(false);
+        // if diff is greater or equal then -3 and less then or equal to 3, its safe
+      } else if (diff !== 0 && diff >= -3 && diff <= 3) {
+        isSafe.push(true);
+      } else {
+        // is diff is greater then 3, its not safe
+        isSafe.push(false);
       }
     }
+  }
+  //   if (c > 0) console.log("arr:> ", arr, isSafe);
+  return isSafe;
+}
+
+function getSafeReports() {
+  const array = challenge2();
+  const safeReports = [];
+  array.forEach((report, index) => {
+    const isSafe = checkSafetyForArray(report);
     if (isSafe.every((e) => e)) safeReports.push(report);
   });
+
   return safeReports.length;
 }
 
