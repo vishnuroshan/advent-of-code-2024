96dafbade162074c8ddbe95979640e22025b9054
Vishnu Roshan <32105473+vishnuroshan@users.noreply.github.com>
2024-12-04 01:03:04 +0530

    Create solution_part_2.js

---
+++ challenge_3/solution_part_2.js
@@ -0,0 +1,32 @@
+const readInput = require("./input");
+const pattern = new RegExp(/mul\(\d{1,3}\,\d{1,3}\)|do\(\)|don't\(\)/g);
+
+function mul(a, b) {
+  return a * b;
+}
+function parseMulFunction() {
+  const str = readInput();
+  return str.match(pattern);
+}
+
+function executeStringExp() {
+  const arr = parseMulFunction();
+  let solution = 0;
+  const stack = [];
+  // using stack
+  arr.forEach((exp) => {
+    if (exp === "don't()") {
+      stack.push("don't");
+    }
+    if (exp === "do()") {
+      stack.length = 0;
+    }
+    if (exp.includes("mul") && stack.length < 1) {
+      console.log(exp);
+      solution = solution + eval(exp);
+    }
+  });
+
+  return solution;
+}
+console.log(executeStringExp());
