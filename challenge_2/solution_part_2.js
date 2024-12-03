305cb58ebc6c2bb79f09087b66d25922bea4e811
Vishnu Roshan <32105473+vishnuroshan@users.noreply.github.com>
2024-12-03 17:57:54 +0530

    Update and rename solution_part_2[WIP].js to solution_part_2.js

--- challenge_2/solution_part_2.js
+++ challenge_2/solution_part_2.js
@@ -3,73 +3,45 @@ const c2 = require("./input");
 const challenge2 = () => {
   let arr = c2.split(/\n/g);
   arr = arr.map((e) => e.split(",").map((e) => parseInt(e)));
-  //   return arr;
-  return [
-    [8, 6, 4, 4, 1],
-    [1, 3, 6, 7, 9],
-    [9, 7, 6, 2, 1],
-    [1, 2, 7, 8, 9],
-  ];
+  return arr;
 };
-
-function problemDampener(arr, i, sign) {
-  console.log(
-    "is sign same",
-    sign === Math.sign(arr[i]),
-    arr,
-    arr[i],
-    " curr:",
-    arr[i] - arr[i + 1],
-    " prev:",
-    arr[i - 1] - arr[i],
-    " next:",
-    arr[i] - arr[i + 2]
-  );
-  const prev = arr[i - 1] - arr[i];
-  const next = arr[i] - arr[i + 2];
-
-  if (sign === Math.sign(prev) || (prev !== 0 && prev >= -3 && prev <= 3)) {
-    return true;
-  }
-  if (sign === Math.sign(next) || (next !== 0 && next >= -3 && next <= 3)) {
-    return true;
-  }
-  return false;
-}
-
 function checkSafetyForArray(array) {
   let arr = [...array];
+  const tArr = [...array];
   const isSafe = [];
   let sign;
   let isDone = false;
+
   for (let i = 0; i < arr.length; i++) {
     if (arr[i + 1] !== undefined) {
       const diff = arr[i] - arr[i + 1];
-      // if no sign assign
       if (!sign) sign = Math.sign(diff);
       if (sign !== Math.sign(diff)) {
-        // if sign changes, then its not safe
-        if (!isDone) {
-          if (problemDampener(arr, i, sign)) isSafe.push(true);
-          isDone = true;
-        } else {
+        if (isDone) {
           isSafe.push(false);
+        } else {
+          const tArr = arr.filter((e) => e !== arr[i + 1]);
+          const isOk = checkSafetyForArray(tArr).every((e) => e);
+          isDone = true;
+          isSafe.push(isOk);
         }
+
         // if diff is greater or equal then -3 and less then or equal to 3, its safe
       } else if (diff !== 0 && diff >= -3 && diff <= 3) {
         isSafe.push(true);
       } else {
         // is diff is greater then 3, its not safe
-        if (!isDone) {
-          if (problemDampener(arr, i, sign)) isSafe.push(true);
-          isDone = true;
-        } else {
+        if (isDone) {
           isSafe.push(false);
+        } else {
+          const tArr = arr.filter((e) => e !== arr[i + 1]);
+          const isOk = checkSafetyForArray(tArr).every((e) => e);
+          isDone = true;
+          isSafe.push(isOk);
         }
       }
     }
   }
-  //   if (c > 0) console.log("arr:> ", arr, isSafe);
   return isSafe;
 }
 
