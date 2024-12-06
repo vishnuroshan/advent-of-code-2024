d4202538d43c362d2da7ec5a308723d6b00fd954
vishnuroshan <vishnuroshan4@gmail.com>
2024-12-07 00:51:55 +0530

    wip

--- challenge_4/solution_part_1.js
+++ challenge_4/solution_part_1.js
@@ -7,21 +7,6 @@ const rules = {
     'S': ['A']
 }
 
-function checkXMAS(arr, row, col, surroundingValArr) {
-    const elem = arr[row][col];
-    surroundingValArr.forEach(e => {
-        const isCurrent = rules[e?.val] && rules[arr[row][col]].includes(e?.val);
-        console.log(`val: ${arr[row][col]}. surrounding val: i:${e.i}, j:${e.j} :${e?.val}, acceptedVals: ${rules[e?.val]}, so: ${isCurrent}`)
-        // if (isCurrent) {
-        //     const surr = getSurroundingVals(arr, e.i, e.j)
-        //     checkXMAS(arr, e.i, e.j, surr);
-        // }
-    })
-    if (arr[row][col] === 'X') {
-        rules['X'].includes(arr[row])
-    }
-    return true;
-}
 
 
 function wordSearch() {
@@ -38,50 +23,93 @@ function wordSearch() {
         ['M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M'],
         ['M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X']
     ];
-
+    let count = 0;
     if (arr.length !== arr[0].length) return `not ok ${arr.length} ${arr[0].length}`;
-
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr[i].length; j++) {
-            if (i === 0 || j === 0 || i === arr.length - 1 || j === arr.length - 1) {
+            if (arr[i][j] === 'X') {
+                const surroundingValToX = getSurroundingVals(arr, i, j).filter((e) => e.val === 'M');
+                console.log('surroundingValToX: ',surroundingValToX)
+                if (surroundingValToX.length > 0) {
+                    for (let x = 0; x < surroundingValToX.length; x++) {
+                        const val = getSurroundingVals(arr, surroundingValToX[x].i, surroundingValToX[x].j);
+                        const surroundingValToM = val.filter((e) => e.val === 'A');
+                        console.log('surroundingValToM: ',surroundingValToM)
+                        for (let b = 0; b < surroundingValToM.length; b++) {
+                            const surroundingValToA = getSurroundingVals(arr, surroundingValToM[b].i, surroundingValToM[b].j).filter((e) => e.val === 'S');
+                            console.log('surroundingValToA: ',surroundingValToA)
+                            for (let s = 0; s < surroundingValToA.length; s++) {
+                                
+                                // const surroundingValToA = getSurroundingVals(arr, s.i, s.j).filter((e) => e.val === 'S');
+                                count++;
+                            }
+    
+                        }
 
-                if (arr[i][j] === 'X' || arr[i][j] === 'S') {
-                    console.log('edge::>', i, j, arr[i][j]);
-                    const surroundingVal = getSurroundingVals(arr, i, j);
-                    checkXMAS(arr, i, j, surroundingVal);
+                    }
                 }
-            } else {
-                console.log('not edge::>', i, j, arr[i][j]);
-                const surroundingVal = getSurroundingVals(arr, i, j);
-                checkXMAS(arr, i, j, surroundingVal);
             }
-
         }
     }
+
+    return count;
 }
+
+
+// function check(arr, i, j, toCheck) {
+//     return getSurroundingVals(arr, i, j).filter((e) => e.val === toCheck);
+// }
+
+// function check(arr, row, col, surroundingValArr, history = ['M', 'A', 'S']) {
+//     const firstPart = [];
+//     const elem = arr[row][col];
+//     if (!history.includes(elem)) {
+//         surroundingValArr.forEach(e => {
+//             const isCurrent = rules[e?.val] && rules[elem].includes(e?.val);
+
+//             if (isCurrent) {
+//                 console.log(`val: ${elem}.\n
+//                 surrounding val: i:${e.i}, j:${e.j} :${e?.val},\n
+//                 acceptedVals: ${rules[elem]},\n
+//                 so: ${isCurrent}`);
+//                 firstPart.push(e);
+//                 // checkX(arr, e.i,e.j,getSurroundingVals(arr, e.i,e.j), [elem])
+//             }
+//         });
+//     }
+//     console.log(firstPart)
+//     return firstPart;
+// }
 
 function getSurroundingVals(arr, i, j) {
     const surroundingVal = [];
-    // if (i === 0 || j === 0 || i === arr.length - 1 || j === arr.length - 1) {
-        // surroundingVal.push({ i: i + 1, j, val: arr[i + 1]?.[j] });
-        // surroundingVal.push({ i: i + 1, j: j + 1, val: arr[i + 1]?.[j + 1] });
-        // surroundingVal.push({ i, j: j + 1, val: arr[i]?.[j + 1] });
+    /**
+     * bottom row
+     * arr[i-1][j+1]
+     * arr[i-1][j]
+     * arr[i-1][j+1]
+     * 
+     * top row
+     * arr[i+1][j-1]
+     * arr[i+1][j]
+     * arr[i+1][j+1]
+     * 
+     * same  row
+     * arr[i][J+1]
+     * arr[i][J-1]
+     */
+    surroundingVal.push({ i: i - 1, j: j - 1, val: arr[i - 1]?.[j - 1] });
+    surroundingVal.push({ i: i - 1, j, val: arr[i - 1]?.[j] });
+    surroundingVal.push({ i: i - 1, j: j + 1, val: arr[i - 1]?.[j + 1] });
+    // middle 2
+    surroundingVal.push({ i, j: j - 1, val: arr[i]?.[j - 1] });
+    surroundingVal.push({ i, j: j + 1, val: arr[i]?.[j + 1] });
 
-    // } else {
-        // top 3
-        surroundingVal.push({ i: i - 1, j: j - 1, val: arr[i - 1]?.[j - 1] });
-        surroundingVal.push({ i: i - 1, j, val: arr[i - 1]?.[j] });
-        surroundingVal.push({ i: i - 1, j: j + 1, val: arr[i - 1]?.[j + 1] });
-        // middle 2
-        surroundingVal.push({ i, j: j - 1, val: arr[i]?.[j - 1] });
-        surroundingVal.push({ i, j: j + 1, val: arr[i]?.[j + 1] });
-
-        surroundingVal.push({ i: i + 1, j: j - 1, val: arr[i + 1]?.[j - 1] });
-        surroundingVal.push({ i: i + 1, j, val: arr[i + 1]?.[j] });
-        surroundingVal.push({ i: i + 1, j: j + 1, val: arr[i + 1]?.[j + 1] });
-
-    // }
-    return surroundingVal.filter(e=>e.val);
+    surroundingVal.push({ i: i + 1, j: j - 1, val: arr[i + 1]?.[j - 1] });
+    surroundingVal.push({ i: i + 1, j, val: arr[i + 1]?.[j] });
+    surroundingVal.push({ i: i + 1, j: j + 1, val: arr[i + 1]?.[j + 1] });
+    console.log(surroundingVal.filter(e => e.val));
+    return surroundingVal.filter(e => e.val && e.val !== arr[i][j]);
 }
 
 console.log(wordSearch())
